fb56a11191c12d358e3052bdedce3f0316df0486
wayne <wayne@campbellscorner.org.uk>
2019-10-22 09:00:31 +0100

    with scalafmt formatting

--- src/main/scala/net/degoes/zio/App10.scala
+++ src/main/scala/net/degoes/zio/App10.scala
@@ -4,60 +4,59 @@ import java.io.IOException
 
 import zio._
 import zio.blocking.Blocking
-import zio.console.{Console, putStrLn, _}
+import zio.console.{ Console, putStrLn, _ }
 
 import scala.util.Try
-
 
 object App10 extends App with Helpers {
   sealed trait Command
 
   object Command {
     final case class ComputeFib(n: Int) extends Command
-    case object Quit extends Command
+    case object Quit                    extends Command
 
-    def fromString(s: String): Option[Command] = {
+    @scala.annotation.tailrec
+    def fromString(s: String): Option[Command] =
       s.trim.toLowerCase() match {
-        case "quit" | "exit" => Some(Quit)
+        case "quit" | "exit"                => Some(Quit)
         case fib if (fib.startsWith("fib")) => fromString(fib.drop(3).trim())
-        case value => Try(value.toInt).toOption.map(ComputeFib(_))
+        case value                          => Try(value.toInt).toOption.map(ComputeFib)
       }
-    }
   }
 
-  final def fib(n:Int): UIO[BigInt] = {
+  final def fib(n: Int): UIO[BigInt] =
     if (n <= 1) UIO(n)
     else fib(n - 2).zipWith(fib(n - 1))(_ + _)
-  }
 
-  def getNumber = getStrLn.flatMap(s => Task(s.toInt)).tapError(_ => putStrLn("You need to enter a number")).eventually
+  def getNumber: ZIO[Console, Nothing, Int] =
+    getStrLn.flatMap(s => Task(s.toInt)).tapError(_ => putStrLn("You need to enter a number")).eventually
 
-  val promptCommand : ZIO[ZEnv, IOException, Command] = {
+  val promptCommand: ZIO[ZEnv, IOException, Command] = {
 
     putStrLn("please enter a command") *>
-      getStrLn.flatMap(line => IO.fromOption(Command.fromString(line)))
-        .tapError(_ => putStrLn(s"enter 'quit' or 'fib <n>'"))
+      getStrLn
+        .flatMap(line => IO.fromOption(Command.fromString(line)))
+        .tapError(_ => putStrLn(s"enter 'quit' or 'fib <n>'"))  // peek at error with interrupting program flow
         .eventually
   }
 
-  override def run(args: List[String]): ZIO[ZEnv, Nothing, Int] = {
+  override def run(args: List[String]): ZIO[ZEnv, Nothing, Int] =
     (for {
-      _ <- putStrLn("How many fibers would you like to use for calculations?")
+      _          <- putStrLn("How many fibers would you like to use for calculations?")
       fiberCount <- getNumber
       // async back pressured queue
       q <- Queue.bounded[Int](3)
       // just describes what a worker does
       worker = (for {
-        n <- q.take
+        n   <- q.take
         num <- fib(n)
-        _ <- putStrLn(s"Fib number $n is $num")
+        _   <- putStrLn(s"Fib number $n is $num")
       } yield ()).forever
 
-      _ <- ZIO.forkAll_(List.fill(fiberCount)(worker))  // make 'fiberCount' copies of worker and fork them all onto different fibers
-      _ <- promptCommand.flatMap{
-        case Command.Quit => ZIO.fail(())
-        case Command.ComputeFib(n) => q.offer(n)
-      }.forever.ignore
+      _ <- ZIO.forkAll_(List.fill(fiberCount)(worker)) // make 'fiberCount' copies of worker and fork them all onto different fibers
+      _ <- promptCommand.flatMap {
+            case Command.Quit          => ZIO.fail(())
+            case Command.ComputeFib(n) => q.offer(n)
+          }.forever.ignore
     } yield ()).exited
-  }
 }
--- src/main/scala/net/degoes/zio/App8.scala
+++ src/main/scala/net/degoes/zio/App8.scala
@@ -1,14 +1,13 @@
 package net.degoes.zio
 
-import java.io.{File, FileInputStream, IOException, InputStream}
+import java.io.{ File, FileInputStream, IOException, InputStream }
 
 import cats.effect.syntax.bracket
 import zio._
-import zio.blocking.{Blocking, effectBlocking}
-import zio.console.{Console, _}
+import zio.blocking.{ effectBlocking, Blocking }
+import zio.console.{ Console, _ }
 
 import scala.util.Try
-
 
 object App8 extends App with Helpers {
 
@@ -17,38 +16,34 @@ object App8 extends App with Helpers {
   // version that converts our option into a ZIO effect
   def parseIntM(s: String): ZIO[Any, Unit, Int] = ZIO.fromOption(parseInt(s))
 
-  def eventually[R,E,A](zio: ZIO[R,E,A]): ZIO[R, Nothing, A] =
-    zio orElse(eventually(zio))
+  def eventually[R, E, A](zio: ZIO[R, E, A]): ZIO[R, Nothing, A] =
+    zio orElse (eventually(zio))
 
-  def retry[R,E,A](n: Int, zio: ZIO[R,E,A]): ZIO[R,E,A] = {
-    if (n <= 1) zio else zio orElse(retry(n - 1, zio))
-  }
-
+  def retry[R, E, A](n: Int, zio: ZIO[R, E, A]): ZIO[R, E, A] =
+    if (n <= 1) zio else zio orElse (retry(n - 1, zio))
 
   // case class with private contructor
   final case class Input private (is: InputStream) {
     val close: IO[IOException, Unit] = IO(is.close()).refineToOrDie[IOException]
     /* .lock(???) lets you restrict the thread pool that an effect is executed on.
-    * ZIO will handle jumping in and out of the restricted context seamlessly */
+     * ZIO will handle jumping in and out of the restricted context seamlessly */
     //    val close: IO[IOException, Unit] = IO(is.close()).refineToOrDie[IOException].lock(???)
 
     // Chunk is essentially an immutable wrapper around an Array
-    def read(size: Int = 1024): ZIO[Blocking, IOException, Option[Chunk[Byte]]] = {
+    def read(size: Int = 1024): ZIO[Blocking, IOException, Option[Chunk[Byte]]] =
       // effectBlocking : run all this on a special blocking thread pool
       // the default context you get with a ZEnv provides a properly configured blocking thread pool
       effectBlocking {
-        val array = new Array[Byte](size)
+        val array     = new Array[Byte](size)
         val bytesRead = is.read(array)
         if (bytesRead == -1) None
         else Some(Chunk.fromArray(array).take(bytesRead))
       }.refineToOrDie[IOException]
-    }
   }
 
   object Input {
-    def open(file: File): IO[IOException, Input] = {
+    def open(file: File): IO[IOException, Input] =
       IO(new Input(new FileInputStream((file)))).refineToOrDie[IOException]
-    }
 
   }
 
@@ -57,11 +52,19 @@ object App8 extends App with Helpers {
       console.putStrLn("need to provide path to a file")
       UIO.succeed(1)
     }
-    Input.open(new File(args.head)).bracket(input => input.close.ignore) { input: Input => {
-      input.read().flatMap {
-        case None => IO.fail(Unit)
-        case Some(chunk) => putStr(chunk.mkString)
-      }.forever orElse IO.unit
-    }}.exited
+    Input
+      .open(new File(args.head))
+      .bracket(input => input.close.ignore) { input: Input =>
+        {
+          input
+            .read()
+            .flatMap {
+              case None        => IO.fail(Unit)
+              case Some(chunk) => putStr(chunk.mkString)
+            }
+            .forever orElse IO.unit
+        }
+      }
+      .exited
   }
 }
